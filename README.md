0d59ce9ab7e102cd58d0b0126e0d19de69a2b1b4
Caio Sanchez <caiosanchezcnd@gmail.com>
2024-09-19 20:16:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# projeto_cardapio
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/projeto_cardapio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projeto_cardapio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
